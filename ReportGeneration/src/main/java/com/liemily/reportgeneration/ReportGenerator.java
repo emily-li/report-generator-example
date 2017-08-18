1e4058026978aface4bcb2af41cd0ced4605acc8
emily-li <emily.li@live.co.uk>
2017-08-19 00:56:46 +0100

    Adding ReportGenerator

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerator.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerator.java
@@ -1,7 +1,31 @@
 package com.liemily.reportgeneration;
 
+import com.liemily.reportgeneration.domain.*;
+import com.liemily.reportgeneration.exceptions.ReportGenerationException;
+import com.liemily.reportgeneration.exceptions.ReportMarshallingException;
+import com.liemily.reportgeneration.exceptions.ReportWritingException;
+import com.liemily.stock.Stock;
+import com.liemily.stock.StockService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.lang.invoke.MethodHandles;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Emily Li on 14/08/2017.
@@ -9,4 +33,80 @@ import org.springframework.stereotype.Component;
 @Component
 @Lazy
 public class ReportGenerator {
+    private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+
+    private StockService stockService;
+
+    @Autowired
+    public ReportGenerator(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    public Report generate(ReportRequest reportRequest) throws ReportGenerationException {
+        ReportItems reportItems = generateReportItems(reportRequest);
+        String reportContents = generateReport(reportItems, reportRequest.getFileType());
+
+        Path reportPath = generateReportPath(reportRequest.getFileType());
+        writeReport(reportContents, reportPath);
+        return new Report(reportPath, reportRequest);
+    }
+
+    private List<Stock> getStocks(Sort.Direction sortDirection) {
+        Sort sort = new Sort(sortDirection, "value");
+        return stockService.getStocks(sort);
+    }
+
+    private ReportItems generateReportItems(ReportRequest reportRequest) {
+        List<ReportItem> reportItems = new ArrayList<>();
+        List<Stock> stocks = new ArrayList<>();
+        if (reportRequest.getReportName().equals(REPORT_NAME.STOCK_REPORT)) {
+            stocks = getStocks(reportRequest.getSortDirection());
+        }
+
+        stocks.forEach(stock -> reportItems.add(new ReportItem(stock.getSymbol(), stock.getSymbol(), stock.getValue(), stock.getVolume(), new BigDecimal(0))));
+        return new ReportItems(reportItems);
+    }
+
+    private Path generateReportPath(FILE_TYPE fileType) {
+        Path path = Paths.get(UUID.randomUUID().toString() + "." + fileType.toString().toLowerCase());
+        path.toFile().deleteOnExit();
+        return path;
+    }
+
+    private void writeReport(String report, Path path) throws ReportWritingException {
+        try {
+            Files.write(path, report.getBytes());
+        } catch (IOException e) {
+            logger.info("Failed to write report to " + path.toAbsolutePath());
+            throw new ReportWritingException("Failed to write report to " + path.toAbsolutePath(), e);
+        }
+        logger.info("Wrote report to " + path.toAbsolutePath());
+    }
+
+    private String generateReport(ReportItems reportItems, FILE_TYPE fileType) throws ReportMarshallingException {
+        String contents;
+        if (fileType.equals(FILE_TYPE.XML)) {
+            contents = generateXML(reportItems);
+        } else {
+            throw new UnsupportedOperationException("Unsupported report file type " + fileType);
+        }
+        return contents;
+    }
+
+    private String generateXML(ReportItems reportItems) throws ReportMarshallingException {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(ReportItems.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            try (StringWriter stringWriter = new StringWriter()) {
+                marshaller.marshal(reportItems, stringWriter);
+                return stringWriter.toString();
+            }
+        } catch (JAXBException | IOException e) {
+            String msg = "Failed to marshal report to string";
+            logger.info(msg, e);
+            throw new ReportMarshallingException(msg, e);
+        }
+    }
 }
---
+++ ReportGeneration/src/test/java/com/liemily/reportgeneration/ReportGeneratorIT.java
@@ -0,0 +1,79 @@
+package com.liemily.reportgeneration;
+
+import com.liemily.reportgeneration.domain.*;
+import com.liemily.stock.Stock;
+import com.liemily.stock.StockRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Emily Li on 14/08/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportGeneratorIT {
+    @Autowired
+    private ReportGenerator reportGenerator;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    private Collection<Stock> repositoryStocks;
+
+    @Before
+    public void setup() {
+        repositoryStocks = new HashSet<>();
+        IntStream.range(0, 3).forEach(i -> repositoryStocks.add(new Stock("SYM" + i, new BigDecimal(i * i), 1)));
+        stockRepository.save(repositoryStocks);
+        repositoryStocks = stockRepository.findAll(); // Required to make the BigDecimal value consistent with the checks that retrieve stocks from the database later
+    }
+
+    @Test
+    public void testGetStockValues() throws Exception {
+        final Sort.Direction[] DIRECTIONS = new Sort.Direction[]{Sort.Direction.ASC, Sort.Direction.DESC};
+        final Map<Sort.Direction, BigDecimal[]> EXPECTED_DIRECTION_VALUES = new HashMap<>();
+
+        TreeSet<BigDecimal> stockValues = new TreeSet<>();
+        repositoryStocks.forEach(stock -> stockValues.add(stock.getValue()));
+
+        EXPECTED_DIRECTION_VALUES.put(Sort.Direction.ASC, stockValues.toArray(new BigDecimal[repositoryStocks.size()]));
+        EXPECTED_DIRECTION_VALUES.put(Sort.Direction.DESC, stockValues.descendingSet().toArray(new BigDecimal[repositoryStocks.size()]));
+
+        for (Sort.Direction direction : DIRECTIONS) {
+            ReportRequest reportRequest = new ReportRequest(REPORT_NAME.STOCK_REPORT, FILE_TYPE.XML, direction);
+            Path reportPath = reportGenerator.generate(reportRequest).getLocation();
+
+            List<Stock> marshalledStocks = getStocksFromXML(reportPath);
+            List<BigDecimal> marshalledStockValues = new ArrayList<>();
+            marshalledStocks.forEach(stock -> marshalledStockValues.add(stock.getValue()));
+
+            assertArrayEquals(EXPECTED_DIRECTION_VALUES.get(direction), marshalledStockValues.toArray(new BigDecimal[repositoryStocks.size()]));
+        }
+    }
+
+    private List<Stock> getStocksFromXML(Path xmlPath) throws Exception {
+        JAXBContext jaxbContext = JAXBContext.newInstance(ReportItems.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        ReportItems reportItems = (ReportItems) unmarshaller.unmarshal(xmlPath.toFile());
+
+        List<Stock> marshalledStocks = new ArrayList<>();
+        reportItems.getReportItemCollection().forEach(reportItem -> marshalledStocks.add(new Stock(reportItem.getStockSymbol(), reportItem.getValue(), reportItem.getVolume())));
+        return marshalledStocks;
+    }
+}
