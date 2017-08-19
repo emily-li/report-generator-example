6ada04f447d0b70b09974c976910c96402908fbf
emily-li <emily.li@live.co.uk>
2017-08-19 01:08:00 +0100

    Decoupling ReportWriter

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerator.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerator.java
@@ -2,8 +2,6 @@ package com.liemily.reportgeneration;
 
 import com.liemily.reportgeneration.domain.*;
 import com.liemily.reportgeneration.exceptions.ReportGenerationException;
-import com.liemily.reportgeneration.exceptions.ReportMarshallingException;
-import com.liemily.reportgeneration.exceptions.ReportWritingException;
 import com.liemily.stock.Stock;
 import com.liemily.stock.StockService;
 import org.apache.log4j.LogManager;
@@ -13,14 +11,8 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import java.io.IOException;
-import java.io.StringWriter;
 import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -36,24 +28,19 @@ public class ReportGenerator {
     private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
     private StockService stockService;
+    private ReportWriter reportWriter;
 
     @Autowired
-    public ReportGenerator(StockService stockService) {
+    public ReportGenerator(StockService stockService, ReportWriter reportWriter) {
         this.stockService = stockService;
+        this.reportWriter = reportWriter;
     }
 
     public Report generate(ReportRequest reportRequest) throws ReportGenerationException {
         ReportItems reportItems = generateReportItems(reportRequest);
-        String reportContents = generateReport(reportItems, reportRequest.getFileType());
-
         Path reportPath = generateReportPath(reportRequest.getFileType());
-        writeReport(reportContents, reportPath);
+        reportWriter.write(reportItems, reportRequest.getFileType(), reportPath);
         return new Report(reportPath, reportRequest);
-    }
-
-    private List<Stock> getStocks(Sort.Direction sortDirection) {
-        Sort sort = new Sort(sortDirection, "value");
-        return stockService.getStocks(sort);
     }
 
     private ReportItems generateReportItems(ReportRequest reportRequest) {
@@ -67,46 +54,14 @@ public class ReportGenerator {
         return new ReportItems(reportItems);
     }
 
+    private List<Stock> getStocks(Sort.Direction sortDirection) {
+        Sort sort = new Sort(sortDirection, "value");
+        return stockService.getStocks(sort);
+    }
+
     private Path generateReportPath(FILE_TYPE fileType) {
         Path path = Paths.get(UUID.randomUUID().toString() + "." + fileType.toString().toLowerCase());
         path.toFile().deleteOnExit();
         return path;
-    }
-
-    private void writeReport(String report, Path path) throws ReportWritingException {
-        try {
-            Files.write(path, report.getBytes());
-        } catch (IOException e) {
-            logger.info("Failed to write report to " + path.toAbsolutePath());
-            throw new ReportWritingException("Failed to write report to " + path.toAbsolutePath(), e);
-        }
-        logger.info("Wrote report to " + path.toAbsolutePath());
-    }
-
-    private String generateReport(ReportItems reportItems, FILE_TYPE fileType) throws ReportMarshallingException {
-        String contents;
-        if (fileType.equals(FILE_TYPE.XML)) {
-            contents = generateXML(reportItems);
-        } else {
-            throw new UnsupportedOperationException("Unsupported report file type " + fileType);
-        }
-        return contents;
-    }
-
-    private String generateXML(ReportItems reportItems) throws ReportMarshallingException {
-        try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(ReportItems.class);
-            Marshaller marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            try (StringWriter stringWriter = new StringWriter()) {
-                marshaller.marshal(reportItems, stringWriter);
-                return stringWriter.toString();
-            }
-        } catch (JAXBException | IOException e) {
-            String msg = "Failed to marshal report to string";
-            logger.info(msg, e);
-            throw new ReportMarshallingException(msg, e);
-        }
     }
 }
